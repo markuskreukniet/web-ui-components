5c6f5e7e5afa3d4e27f724fb88522f6dc31caa23
Markus Kreukniet <shonof@gmail.com>
2025-09-06 10:58:42 +0200

    WIP

--- packages/ui-solid/src/components/FilePanelSwitcher.tsx
+++ packages/ui-solid/src/components/FilePanelSwitcher.tsx
@@ -1,7 +1,3 @@
-import { FilePathSelectionForm } from './FilePathSelectionForm'
-import { FileResultInspector } from './FileResultInspector'
-import { createStep, Stepper } from './Stepper'
-import { isArrayEmpty } from '../utils/isEmpty'
 import type { Component } from 'solid-js'
 import type { FilePathSelectionFormBaseProps, OnChangeSourceTargetContextEither } from './FilePathSelectionForm'
 import type { CanDeleteProps } from './FileResultInspector'
@@ -16,39 +12,5 @@ type FilePanelSwitcherProps =
 // TODO: this component is useless? When removing also check props above here
 // TODO: also remove SegmentedControl component?
 export const FilePanelSwitcher: Component<FilePanelSwitcherProps> = props => {
-  return (
-    <Stepper
-      steps={[
-        createStep(
-          'File Selection',
-          'Select file(s) and/or folder(s)',
-          'Choose file(s) and/or folder(s) that you want to check for duplicates.',
-          <FilePathSelectionForm
-            filePathSelectorMode={props.filePathSelectorMode}
-            isLoading={props.isLoading}
-            singleSelection={props.singleSelection}
-            enableTargetSelection={props.enableTargetSelection}
-            selectFilePath={props.selectFilePath}
-            onChange={props.onChangeSourceTargetContextEither}
-          />,
-          'Add file(s) and/or folder(s) to continue'
-        ),
-        createStep(
-          'File Inspection',
-          '',
-          '',
-          <FileResultInspector
-            columns={props.columns}
-            rowGroups={props.rowGroups}
-            isLoading={props.isLoading}
-            canDelete={props.canDelete}
-            onChange={props.onChangeSelectedGroupRows}
-          />,
-          ''
-        )
-      ]}
-      lastEnabledStepIndex={isArrayEmpty(props.rowGroups) ? 0 : 1}
-      showNavigationControls={false}
-    />
-  )
+  return (<></>)
 }
