08a5bc10bde979ab56e11d58027318fcaf3fcc45
markuskreukniet <shonof@gmail.com>
2025-09-11 21:03:55 +0200

    WIP

--- packages/ui-solid/src/components/FilePanelSwitcher.tsx
+++
@@ -1,8 +0,0 @@
-import type { Component } from 'solid-js'
-
-type FilePanelSwitcherProps = {}
-
-// TODO: this component is useless? When removing also check props above here
-export const FilePanelSwitcher: Component<FilePanelSwitcherProps> = props => {
-  return (<></>)
-}
